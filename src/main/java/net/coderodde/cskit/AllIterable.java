5addaf31155e6e678474c1f1891504c001845036
oevevr <120677373+oevevr@users.noreply.github.com>
2020-10-31 07:51:34 +0000

    Updated !

--- src/main/java/net/coderodde/cskit/AllIterable.java
+++ src/main/java/net/coderodde/cskit/AllIterable.java
@@ -2,4 +2,7 @@ package net.coderodde.cskit;
 
 /**
  * This interface defines the API for iterating over all nodes connected to
- * the object implementing this interface,
+ * the object implementing this interface, in both directions.
+ *
+ * @author Rodion Efremov
+ * @version 1.6
