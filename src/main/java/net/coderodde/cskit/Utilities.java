bb3a146611421c562922279d870c5c513ee9e82f
oevevr <120677373+oevevr@users.noreply.github.com>
2020-11-20 21:19:34 +0000

    Update cli.php

--- src/main/java/net/coderodde/cskit/Utilities.java
+++ src/main/java/net/coderodde/cskit/Utilities.java
@@ -218,3 +218,94 @@ public class Utilities {
             if (paths[i].size() != paths[i + 1].size()) {
                 return false;
             }
+        }
+
+        for (int i = 0; i < paths[0].size(); ++i) {
+            for (int p = 0; p < paths.length - 1; ++p) {
+                if (paths[p].get(i).equals(paths[p + 1].get(i)) == false) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public static final List<DirectedGraphNode> generateSimpleGraph(int size, float edgeLoadFactor, Random r) {
+        List<DirectedGraphNode> graph = new ArrayList<DirectedGraphNode>(size);
+
+        for (int i = 0; i < size; ++i) {
+            graph.add(new DirectedGraphNode("" + i));
+        }
+
+        for (int i = 0; i < size; ++i) {
+            for (int j = 0; j < size; ++j) {
+                if (r.nextFloat() < edgeLoadFactor) {
+                    graph.get(i).addChild(graph.get(j));
+                }
+            }
+        }
+
+        for (int i = 0; i < size - 1; ++i) {
+            graph.get(i).addChild(graph.get(i + 1));
+        }
+
+        graph.get(graph.size() - 1).addChild(graph.get(0));
+
+        return graph;
+    }
+
+    public static final Pair<List<DirectedGraphNode>, DirectedGraphWeightFunction>
+            getWeightedGraph(int size, float elf, Random r) {
+        List<DirectedGraphNode> graph = new ArrayList<DirectedGraphNode>(size);
+        DirectedGraphWeightFunction w = new DirectedGraphWeightFunction();
+
+        for (int i = 0; i < size; ++i) {
+            graph.add(new DirectedGraphNode("" + i));
+        }
+
+        for (int i = 0; i < size; ++i) {
+            for (int j = 0; j < size; ++j) {
+                if (r.nextFloat() < elf) {
+                    graph.get(i).addChild(graph.get(j));
+                    w.put(graph.get(i), graph.get(j), 10.0 * r.nextDouble());
+                }
+            }
+        }
+
+        for (int i = 0; i < size - 1; ++i) {
+            graph.get(i).addChild(graph.get(i + 1));
+            w.put(graph.get(i), graph.get(i + 1), 10.0 * r.nextDouble());
+        }
+
+        graph.get(graph.size() - 1).addChild(graph.get(0));
+        w.put(graph.get(graph.size() - 1), graph.get(0), 1.0 * r.nextDouble());
+
+        return new Pair<List<DirectedGraphNode>, DirectedGraphWeightFunction>
+                   (graph, w);
+    }
+
+    public static final boolean isSpanningTree(List<UndirectedGraphEdge> tree) {
+        Set<UndirectedGraphNode> set = new HashSet<UndirectedGraphNode>();
+
+        for (UndirectedGraphEdge e : tree) {
+            set.add(e.getA());
+            set.add(e.getB());
+        }
+
+        return set.size() > tree.size();
+    }
+
+    public static final double
+            sumEdgeWeights(List<UndirectedGraphEdge> edges) {
+        double sum = 0.0;
+
+        for (UndirectedGraphEdge e : edges) {
+            sum += e.getWeight();
+        }
+
+        return sum;
+    }
+
+    public static final boolean
+            spanningTreesEqual(List<UndirectedGraphEdge> tree1,
