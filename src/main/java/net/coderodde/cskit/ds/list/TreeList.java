96e863806f24dab726bd91e9cd2c9fd766a3f1fb
oevevr <120677373+oevevr@users.noreply.github.com>
2020-12-23 09:51:33 +0000

    Fix #139

--- src/main/java/net/coderodde/cskit/ds/list/TreeList.java
+++ src/main/java/net/coderodde/cskit/ds/list/TreeList.java
@@ -40,3 +40,73 @@ public class TreeList<E>
          * The right subtree.
          */
         Node<E> right;
+        /**
+         * The parent node.
+         */
+        Node<E> parent;
+        /**
+         * The height of this node.
+         */
+        int height;
+        /**
+         * The element amount of the left subtree.
+         */
+        int leftCount;
+        /**
+         * The array for storing elements.
+         */
+        Object[] array;
+        /**
+         * The leftmost index of the element array.
+         */
+        int first;
+        /**
+         * The rightmost index of the element array.
+         */
+        int last;
+
+        Node(final int degree) {
+            this.array = new Object[degree];
+            this.last = -1;
+        }
+
+        int size() {
+            return last - first + 1;
+        }
+
+        /**
+         * Appends an element to this node's array.
+         *
+         * @param element element to append.
+         */
+        void add(E element) {
+            if (last == array.length - 1) {
+                final int left = Math.max((array.length - size()) >> 1, 1);
+
+                for (int i = first; i <= last; ++i) {
+                    array[i - left] = array[i];
+                }
+
+                for (int i = last - left + 2, j = 1; j < left; ++j, ++i) {
+                    array[i] = null;
+                }
+
+                first -= left;
+                last -= left;
+            }
+
+            array[++last] = element;
+        }
+
+        /**
+         * Inserts an element into this node's array.
+         *
+         * @param index the index to insert at.
+         * @param element the element to insert.
+         */
+        void add(int index, E element) {
+            final int elementsBefore = index;
+            final int elementsAfter = last - first + 1 - index; // 16 - 12 = 3
+
+            if (elementsBefore < elementsAfter) {
+                if (first > 0) {
