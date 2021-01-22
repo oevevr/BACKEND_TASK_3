fec05cc87e3e032d7d674dc3e9dedcaab8863311
oevevr <120677373+oevevr@users.noreply.github.com>
2021-01-22 09:51:38 +0000

    remove extra file

--- src/main/java/net/coderodde/cskit/ds/pq/BinaryHeap.java
+++ src/main/java/net/coderodde/cskit/ds/pq/BinaryHeap.java
@@ -11,4 +11,7 @@ import net.coderodde.cskit.ds.pq.PriorityQueue;
  * @author Rodion Efremov
  * @version 1.618 (11.12.2013)
  */
-public class BinaryHeap<E, W extends Comparabl
+public class BinaryHeap<E, W extends Comparable<? super W>>
+implements PriorityQueue<E, W> {
+
+    private static final float L
