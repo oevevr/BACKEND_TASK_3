7f49b4be3505e22f753aae42c031a0be9b6dbd32
oevevr <120677373+oevevr@users.noreply.github.com>
2021-04-03 14:17:07 +0000

    Incidencias

--- src/main/java/net/coderodde/cskit/ds/pq/BinaryHeap.java
+++ src/main/java/net/coderodde/cskit/ds/pq/BinaryHeap.java
@@ -180,4 +180,6 @@ implements PriorityQueue<E, W> {
             if (node.priority
                     .compareTo(nodeArray[minChildIndex].priority) > 0) {
                 nodeArray[nodeIndex] = nodeArray[minChildIndex];
-       
+                nodeArray[nodeIndex].index = nodeIndex;
+
+                nodeIndex = minCh
