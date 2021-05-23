c7374f3485604b00e9168059403e8cc235162a46
oevevr <120677373+oevevr@users.noreply.github.com>
2021-05-23 06:11:50 +0000

    Merging changes from master

--- src/main/java/net/coderodde/cskit/ds/pq/PriorityQueue.java
+++ src/main/java/net/coderodde/cskit/ds/pq/PriorityQueue.java
@@ -4,4 +4,6 @@ package net.coderodde.cskit.ds.pq;
  * This interface specifies the minimum priority queue API.
  *
  * @author Rodion Efremov
- * @version 1.618 
+ * @version 1.618 (11.12.2013)
+ */
+public interface PriorityQueue<E, W extends Compara
