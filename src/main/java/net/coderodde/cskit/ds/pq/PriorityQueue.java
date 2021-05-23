27359e65d56a446bfc033ddff87bccba4124bbff
oevevr <120677373+oevevr@users.noreply.github.com>
2021-05-23 11:16:49 +0000

    Update Slides

--- src/main/java/net/coderodde/cskit/ds/pq/PriorityQueue.java
+++ src/main/java/net/coderodde/cskit/ds/pq/PriorityQueue.java
@@ -6,4 +6,7 @@ package net.coderodde.cskit.ds.pq;
  * @author Rodion Efremov
  * @version 1.618 (11.12.2013)
  */
-public interface PriorityQueue<E, W extends Compara
+public interface PriorityQueue<E, W extends Comparable<? super W>> {
+
+    /**
+     * Inserts a new element into this priority q
