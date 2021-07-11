7de55a12292eac294ea8395fe5a38349ccdb31f1
oevevr <120677373+oevevr@users.noreply.github.com>
2021-07-11 10:30:29 +0000

    Code repalced

--- src/main/java/net/coderodde/cskit/ds/tree/OrderStatisticTree.java
+++ src/main/java/net/coderodde/cskit/ds/tree/OrderStatisticTree.java
@@ -21,3 +21,84 @@ public class OrderStatisticTree<K extends Comparable<? super K>, V>
     /**
      * An entry (a node) in this tree.
      *
+     * @param <K> the type of keys.
+     * @param <V> the type of values.
+     */
+    public static class Node<K, V> implements Map.Entry<K, V> {
+
+        /**
+         * The key of this entry.
+         */
+        private K key;
+
+        /**
+         * The value of this entry.
+         */
+        private V value;
+
+        /**
+         * Amount of all key-value -mappings in the left subtree of this entry.
+         */
+        private int count;
+
+        /**
+         * This field is the height of this entry. Grows upwards.
+         */
+        private int h;
+
+        /**
+         * The parent entry of this entry.
+         */
+        private Node<K, V> parent;
+
+        /**
+         * The left entry (subtree).
+         */
+        private Node<K, V> left;
+
+        /**
+         * The right entry.
+         */
+        private Node<K, V> right;
+
+        /**
+         * Constructs a new tree node with the specified data.
+         *
+         * @param key the key of a newly created node.
+         * @param value the value of a newly created node.
+         */
+        Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        /**
+         * Returns the key of this entry.
+         *
+         * @return the key of this entry.
+         */
+        @Override
+        public K getKey() {
+            return key;
+        }
+
+        /**
+         * Returns the value of this entry.
+         *
+         * @return the value of this entry.
+         */
+        @Override
+        public V getValue() {
+            return value;
+        }
+
+        /**
+         * Set the value and returns the old one.
+         *
+         * @param value the value to set.
+         *
+         * @return the old value.
+         */
+        @Override
+        public V setValue(V value) {
+            V old = this.value;
