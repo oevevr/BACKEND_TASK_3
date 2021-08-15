805074714520b630a0803e8afb6cded876b62e46
oevevr <120677373+oevevr@users.noreply.github.com>
2021-08-15 06:24:00 +0000

    PR

--- src/main/java/net/coderodde/cskit/graph/UndirectedGraphEdge.java
+++ src/main/java/net/coderodde/cskit/graph/UndirectedGraphEdge.java
@@ -5,4 +5,8 @@ import java.util.Comparator;
 /**
  * This class holds
  * @author Rodion Efremov
- * @version 1.618033 (28.12.
+ * @version 1.618033 (28.12.2013)
+ */
+public class UndirectedGraphEdge {
+
+    private UndirectedGraphN
