3a767d0aab04c20360236d8f595c6a1b3ffd7315
oevevr <120677373+oevevr@users.noreply.github.com>
2021-09-12 11:55:19 +0000

    small refinements

--- src/main/java/net/coderodde/cskit/graph/UndirectedGraphEdge.java
+++ src/main/java/net/coderodde/cskit/graph/UndirectedGraphEdge.java
@@ -56,4 +56,7 @@ public class UndirectedGraphEdge {
         public int compare(UndirectedGraphEdge o1, UndirectedGraphEdge o2) {
             if (o1.w < o2.w) {
                 return -1;
-            } else i
+            } else if (o1.w > o2.w) {
+                return 1;
+            } else {
+                return
