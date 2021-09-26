fb3e807bf68cb758f7bacde728f418836cef43ab
oevevr <120677373+oevevr@users.noreply.github.com>
2021-09-26 20:32:47 +0000

    Updated Finnish translations.

--- src/main/java/net/coderodde/cskit/graph/UndirectedGraphNode.java
+++ src/main/java/net/coderodde/cskit/graph/UndirectedGraphNode.java
@@ -2,4 +2,6 @@ package net.coderodde.cskit.graph;
 
 import java.util.Iterator;
 import java.util.LinkedHashSet;
-import java.util.NoSuch
+import java.util.NoSuchElementException;
+import java.util.Set;
+import static net.coderodde.cskit.Utilities.checkNotNull;
