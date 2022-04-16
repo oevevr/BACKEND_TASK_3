4e42bf3739dc01e1147a8d21569e55786d195b21
oevevr <120677373+oevevr@users.noreply.github.com>
2022-04-16 18:39:28 +0000

    Trying

--- src/main/java/net/coderodde/cskit/graph/p2psp/general/ChebyshevMetric.java
+++ src/main/java/net/coderodde/cskit/graph/p2psp/general/ChebyshevMetric.java
@@ -3,4 +3,7 @@ package net.coderodde.cskit.graph.p2psp.general;
 import net.coderodde.cskit.graph.DirectedGraphNode;
 
 /**
- * This class provides a metric for heu
+ * This class provides a metric for heuristic search based on Tshebyshev
+ * distance metric.
+ *
+ * @author Rod
