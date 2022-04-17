387e0ff0631864943bcd74842e2762112d64cb75
oevevr <120677373+oevevr@users.noreply.github.com>
2022-04-17 08:14:31 +0000

    A little refactoring

--- src/main/java/net/coderodde/cskit/graph/p2psp/general/ChebyshevMetric.java
+++ src/main/java/net/coderodde/cskit/graph/p2psp/general/ChebyshevMetric.java
@@ -6,4 +6,7 @@ import net.coderodde.cskit.graph.DirectedGraphNode;
  * This class provides a metric for heuristic search based on Tshebyshev
  * distance metric.
  *
- * @author Rod
+ * @author Rodion Efremov
+ * @version 1.618 (16.12.2013)
+ */
+public class 
