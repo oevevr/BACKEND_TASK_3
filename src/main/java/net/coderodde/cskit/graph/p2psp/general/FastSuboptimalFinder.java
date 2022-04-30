769f0e92ffd6d56786bb1203e205859ce1fd153d
oevevr <120677373+oevevr@users.noreply.github.com>
2022-04-30 18:14:16 +0000

    [NPM-BOT] Update dependencies ...

--- src/main/java/net/coderodde/cskit/graph/p2psp/general/FastSuboptimalFinder.java
+++ src/main/java/net/coderodde/cskit/graph/p2psp/general/FastSuboptimalFinder.java
@@ -57,3 +57,81 @@ public class FastSuboptimalFinder extends GeneralPathFinder {
         PARENT_MAP2.clear();
 
         OPEN.insert(source, h.get(source));
+        PARENT_MAP.put(source, null);
+        GSCORE_MAP.put(source, 0.0);
+
+        OPEN2.insert(target, h2.get(target));
+        PARENT_MAP2.put(target, null);
+        GSCORE_MAP2.put(target, 0.0);
+
+        double m = Double.POSITIVE_INFINITY;
+        DirectedGraphNode touch = null;
+
+        while ((OPEN.isEmpty() == false) && (OPEN2.isEmpty() == false)) {
+            if (touch != null) {
+                double L1 = GSCORE_MAP.get(touch);
+                double L2 = GSCORE_MAP2.get(touch);
+
+                if (m >= L1 + L2) {
+                    return tracebackPathBidirectional(touch,
+                                                      PARENT_MAP,
+                                                      PARENT_MAP2);
+                }
+            }
+
+            DirectedGraphNode current = OPEN.extractMinimum();
+            CLOSED.add(current);
+
+            for (DirectedGraphNode child : current) {
+                if (CLOSED.contains(child)) {
+                    continue;
+                }
+
+                double tmpg = GSCORE_MAP.get(current) + w.get(current, child);
+
+                if (GSCORE_MAP.containsKey(child) == false) {
+                    OPEN.insert(child, tmpg + h.get(child));
+                    GSCORE_MAP.put(child, tmpg);
+                    PARENT_MAP.put(child, current);
+
+                    if (CLOSED2.contains(child)) {
+                        if (m > tmpg + GSCORE_MAP2.get(child)) {
+                            m = tmpg + GSCORE_MAP2.get(child);
+                            touch = child;
+                        }
+                    }
+                } else if (tmpg < GSCORE_MAP.get(child)) {
+                    OPEN.decreasePriority(child, tmpg + h.get(child));
+                    GSCORE_MAP.put(child, tmpg);
+                    PARENT_MAP.put(child, current);
+
+                    if (CLOSED2.contains(child)) {
+                        if (m > tmpg + GSCORE_MAP2.get(child)) {
+                            m = tmpg + GSCORE_MAP2.get(child);
+                            touch = child;
+                        }
+                    }
+                }
+            }
+
+            current = OPEN2.extractMinimum();
+            CLOSED2.add(current);
+
+            for (DirectedGraphNode parent : current.parentIterable()) {
+                if (CLOSED2.contains(parent)) {
+                    continue;
+                }
+
+                double tmpg = GSCORE_MAP2.get(current) + w.get(parent, current);
+
+                if (GSCORE_MAP2.containsKey(parent) == false) {
+                    OPEN2.insert(parent, tmpg + h2.get(parent));
+                    GSCORE_MAP2.put(parent, tmpg);
+                    PARENT_MAP2.put(parent, current);
+
+                    if (CLOSED.contains(parent)) {
+                        if (m > tmpg + GSCORE_MAP.get(parent)) {
+                            m = tmpg + GSCORE_MAP.get(parent);
+                            touch = parent;
+                        }
+                    }
