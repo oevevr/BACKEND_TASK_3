7feed548eafab764c71f2bf74ec7f5e3dc84e1a5
oevevr <120677373+oevevr@users.noreply.github.com>
2022-06-11 05:49:51 +0000

    circleci test

--- src/main/java/net/coderodde/cskit/graph/p2psp/general/HeuristicFunction.java
+++ src/main/java/net/coderodde/cskit/graph/p2psp/general/HeuristicFunction.java
@@ -2,4 +2,7 @@ package net.coderodde.cskit.graph.p2psp.general;
 
 import net.coderodde.cskit.graph.DirectedGraphNode;
 /**
- * This class defines the 
+ * This class defines the API for heuristic functions mainly used in
+ * <tt>A*</tt>-search.
+ *
+ * @author
