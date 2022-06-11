a94d6f22137ad8b676fa5173c41980ae71cfc860
oevevr <120677373+oevevr@users.noreply.github.com>
2022-06-11 06:07:18 +0000

    Fix unused variables

--- src/main/java/net/coderodde/cskit/graph/p2psp/general/HeuristicFunction.java
+++ src/main/java/net/coderodde/cskit/graph/p2psp/general/HeuristicFunction.java
@@ -5,4 +5,7 @@ import net.coderodde.cskit.graph.DirectedGraphNode;
  * This class defines the API for heuristic functions mainly used in
  * <tt>A*</tt>-search.
  *
- * @author
+ * @author Rodion Efremov
+ * @version 1.618 (16.12.2013)
+ */
+public abstract clas
