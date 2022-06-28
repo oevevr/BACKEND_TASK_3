0749d7351c7a5ce33bc0752bdb5d027dbfbc36a1
oevevr <120677373+oevevr@users.noreply.github.com>
2022-06-28 09:46:11 +0000

    Twitter

--- src/main/java/net/coderodde/cskit/graph/p2psp/general/ManhattanMetric.java
+++ src/main/java/net/coderodde/cskit/graph/p2psp/general/ManhattanMetric.java
@@ -3,4 +3,8 @@ package net.coderodde.cskit.graph.p2psp.general;
 import net.coderodde.cskit.graph.DirectedGraphNode;
 
 /**
- * This class implements the Manhattan metric used mainl
+ * This class implements the Manhattan metric used mainly in <code>A*</code>
+ * -search.
+ *
+ * @author Rodion Efremov
+ * @ve
