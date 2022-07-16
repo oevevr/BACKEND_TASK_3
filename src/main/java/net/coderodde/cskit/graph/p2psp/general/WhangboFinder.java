fba620d7d91b0ee88773b503417313b5f8053aee
oevevr <120677373+oevevr@users.noreply.github.com>
2022-07-16 10:23:56 +0000

    mfd: adp5520: Restore mode bits on resume

--- src/main/java/net/coderodde/cskit/graph/p2psp/general/WhangboFinder.java
+++ src/main/java/net/coderodde/cskit/graph/p2psp/general/WhangboFinder.java
@@ -67,3 +67,74 @@ public class WhangboFinder extends GeneralPathFinder {
         GSCORE_MAP.clear();
         PARENT_MAP.clear();
         OPENL1.clear();
+
+        h2.setTarget(source);
+        OPEN2.clear();
+        CLOSED2.clear();
+        GSCORE_MAP2.clear();
+        PARENT_MAP2.clear();
+        OPENL2.clear();
+
+        OPEN.insert(source, h.get(source));
+        PARENT_MAP.put(source, null);
+        GSCORE_MAP.put(source, 0.0);
+
+        OPEN2.insert(target, h2.get(target));
+        PARENT_MAP2.put(target, null);
+        GSCORE_MAP2.put(target, 0.0);
+
+        double m = Double.POSITIVE_INFINITY;
+
+        while ((OPEN.isEmpty() == false) && (OPEN2.isEmpty() == false)) {
+
+            if (touch != null) {
+                double L1 = GSCORE_MAP.get(touch);
+                double L2 = GSCORE_MAP2.get(touch);
+
+                if (L1 <= OPENL1.getPriority(OPENL1.min())
+                        && L2 <= OPENL2.getPriority(OPENL2.min())) {
+                    return tracebackPathBidirectional(touch,
+                                                      PARENT_MAP,
+                                                      PARENT_MAP2);
+                }
+            }
+
+            DirectedGraphNode current = OPEN.extractMinimum();
+            CLOSED.add(current);
+
+            for (DirectedGraphNode child : current) {
+                if (CLOSED.contains(child)) {
+                    continue;
+                }
+
+                double tmpg = GSCORE_MAP.get(current) + w.get(current, child);
+
+                if (GSCORE_MAP.containsKey(child) == false) {
+                    OPEN.insert(child, tmpg);
+                    GSCORE_MAP.put(child, tmpg);
+                    PARENT_MAP.put(child, current);
+
+                    if (CLOSED2.contains(child)) {
+                        if (m > tmpg + GSCORE_MAP2.get(child)) {
+                            m = tmpg + GSCORE_MAP2.get(child);
+
+                            if (touch == null) {
+                                separator = child;
+                            }
+
+                            touch = child;
+                            OPENL1.insert(child, tmpg + l1(child));
+                            OPENL2.insert(child, GSCORE_MAP2.get(child) + l2(child));
+                        }
+                    }
+                } else if (tmpg < GSCORE_MAP.get(child)) {
+                    OPEN.decreasePriority(child, tmpg);
+                    GSCORE_MAP.put(child, tmpg);
+                    PARENT_MAP.put(child, current);
+
+                    if (CLOSED2.contains(child)) {
+                        if (m > tmpg + GSCORE_MAP2.get(child)) {
+                            m = tmpg + GSCORE_MAP2.get(child);
+
+                            if (touch == null) {
+                                separator = child;
