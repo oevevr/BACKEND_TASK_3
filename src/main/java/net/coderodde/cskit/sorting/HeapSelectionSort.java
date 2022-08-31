15389eb5988291ea1ba9576401e032b6004270c0
oevevr <120677373+oevevr@users.noreply.github.com>
2022-08-31 18:18:42 +0000

    Remove unneeded comments from role defaults

--- src/main/java/net/coderodde/cskit/sorting/HeapSelectionSort.java
+++ src/main/java/net/coderodde/cskit/sorting/HeapSelectionSort.java
@@ -17,3 +17,45 @@ implements ObjectSortingAlgorithm<E> {
     public void sort(E[] array) {
         sort(array, new Range(0, array.length - 1));
     }
+
+    @Override
+    public void sort(E[] array, Range r) {
+        if (r.from <= r.to) {
+            ascendingSort(array, r.from, r.to);
+        } else {
+            descendingSort(array, r.to, r.from);
+        }
+    }
+
+    private void ascendingSort(E[] array, int from, int to) {
+        E[] buffer = array.clone();
+
+        RunHeap<E> heap = new RunScanner<E>()
+                .scanAndReturnRunHeap(buffer, from, to);
+
+        int index = 0;
+        final int N = to - from + 1;
+
+        while (index < N - 1) {
+            array[index++] = heap.min();
+            heap.inc();
+        }
+
+        array[index] = heap.min();
+    }
+
+    private void descendingSort(E[] array, int from, int to) {
+        E[] buffer = array.clone();
+
+        RunHeap<E> heap = new RunScanner<E>()
+                .scanAndReturnRunHeap(array, to, from);
+
+        int index = 0;
+        final int N = to - from + 1;
+
+        while (index < N) {
+            array[index++] = heap.min();
+            heap.inc();
+        }
+    }
+}
