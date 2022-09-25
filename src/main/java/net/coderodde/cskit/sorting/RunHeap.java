b14fa6432079d45861e26b7382210d561dd352d7
oevevr <120677373+oevevr@users.noreply.github.com>
2022-09-25 22:30:01 +0000

    Some changes.

--- src/main/java/net/coderodde/cskit/sorting/RunHeap.java
+++ src/main/java/net/coderodde/cskit/sorting/RunHeap.java
@@ -3,3 +3,7 @@ package net.coderodde.cskit.sorting;
 /**
  * This class implements a run heap for the sake of heap-selection sort.
  *
+ * @author Rodion Efremov
+ * @version 1.618 (14.12.2013)
+ */
+c
