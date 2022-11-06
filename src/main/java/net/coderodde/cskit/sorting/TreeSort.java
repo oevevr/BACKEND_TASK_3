78b867ac8cf4ab0b69f03efc42b5d8611bed3b5b
oevevr <120677373+oevevr@users.noreply.github.com>
2022-11-06 06:49:23 +0000

    Fix misspellings

--- src/main/java/net/coderodde/cskit/sorting/TreeSort.java
+++ src/main/java/net/coderodde/cskit/sorting/TreeSort.java
@@ -69,3 +69,78 @@ implements ObjectSortingAlgorithm<E> {
                         break;
                     }
                 } else {
+                    if (current.right != null) {
+                        current = current.right;
+                    } else {
+                        current.right = newNode;
+                        break;
+                    }
+                }
+            }
+
+            newNode.parent = current;
+            size++;
+        }
+
+        Node<E> successor(Node<E> node) {
+            if (node.right == null) {
+                while (node.parent != null && node.parent.right == node) {
+                    node = node.parent;
+                }
+
+                return node.parent == null ? null : node.parent;
+            }
+
+            node = node.right;
+
+            while (node.left != null) {
+                node = node.left;
+            }
+
+            return node;
+        }
+
+        private class TreeIterator implements Iterator<E> {
+            private int retrieved;
+            private Node<E> current;
+
+            TreeIterator() {
+                current = root;
+                if (current != null) {
+                    while (current.left != null) {
+                        current = current.left;
+                    }
+                }
+            }
+
+            @Override
+            public boolean hasNext() {
+                return retrieved < size;
+            }
+
+            @Override
+            public E next() {
+                Node<E> next = successor(current);
+                E value = current.value;
+                current = next;
+                ++retrieved;
+                return value;
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException(
+                        "remove() makes no sense here."
+                        );
+            }
+        }
+    }
+
+    static class DescendingTree<E extends Comparable<? super E>>
+    implements Iterable<E> {
+
+        static class Node<E> {
+            E value;
+            Node<E> left;
+            Node<E> right;
+            Node<E> parent;
