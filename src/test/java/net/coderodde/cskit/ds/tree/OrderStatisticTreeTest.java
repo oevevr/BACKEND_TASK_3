503564f7fc3404d76c52716641e19f87ae3ae2e0
oevevr <120677373+oevevr@users.noreply.github.com>
2022-12-07 06:16:44 +0000

    Linux 3.10.49

--- src/test/java/net/coderodde/cskit/ds/tree/OrderStatisticTreeTest.java
+++ src/test/java/net/coderodde/cskit/ds/tree/OrderStatisticTreeTest.java
@@ -26,3 +26,63 @@ public class OrderStatisticTreeTest {
         assertEquals(100, tree.size());
 
         for (Integer i = 0; i < tree.size(); ++i) {
+            assertEquals(i, tree.get(i));
+            assertEquals(i, tree.entryAt(i).getKey());
+            assertEquals(i, tree.entryAt(i).getValue());
+            assertEquals(i, new Integer(tree.rankOf(new Integer(i))));
+        }
+
+        for (Integer i = 20; i < 100; ++i) {
+            tree.remove(i);
+        }
+
+        assertEquals(20, tree.size());
+    }
+
+    @Test
+    public void test2() {
+        tree.clear();
+
+        assertTrue(tree.isEmpty());
+        assertEquals(0, tree.size());
+
+        for (int i = 0; i < 20; i++) {
+            tree.put(i, i);
+        }
+
+        int i = 0;
+        Integer j;
+        Iterator<Integer> iterator = tree.iterator();
+
+        while (iterator.hasNext()) {
+            j = iterator.next();
+
+            if (j == 9 || j == 19) {
+                iterator.remove();
+            }
+
+            i++;
+        }
+
+        assertEquals(18, tree.size());
+        assertFalse(tree.isEmpty());
+
+        for (i = 0; i < 20; ++i) {
+            if (i == 9 || i == 19) {
+                assertFalse(tree.containsKey(i));
+            } else {
+                assertTrue(tree.containsKey(i));
+            }
+        }
+
+        assertTrue(tree.isHealthy());
+
+        for (i = 0; i < 20; ++i) {
+            tree.remove(i);
+        }
+
+        assertTrue(tree.isEmpty());
+        assertTrue(tree.isHealthy());
+        assertEquals(0, tree.size());
+
+        tree.put(1, 1);
