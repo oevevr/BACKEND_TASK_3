6f941c0ba30481eaaf4d8b799d11a9cff8cac166
oevevr <120677373+oevevr@users.noreply.github.com>
2022-12-12 19:01:06 +0000

    Initial Dockerfile

--- src/test/java/net/coderodde/cskit/graph/flow/EdmondKarpFlowFinderTest.java
+++ src/test/java/net/coderodde/cskit/graph/flow/EdmondKarpFlowFinderTest.java
@@ -4,4 +4,7 @@ import net.coderodde.cskit.Utilities.Pair;
 import net.coderodde.cskit.graph.DirectedGraphNode;
 import net.coderodde.cskit.graph.DirectedGraphWeightFunction;
 import org.junit.Test;
-import static or
+import static org.junit.Assert.*;
+
+/**
+ * This class tests Edmond-Karp's maximum flow algorith
