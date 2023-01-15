3d8b8d4e91c774f9de2b7e55ea2c595e4e54bf22
oevevr <120677373+oevevr@users.noreply.github.com>
2023-01-15 12:39:32 +0000

    Update TemplateTest.php

--- src/test/java/net/coderodde/cskit/sorting/RunScannerTest.java
+++ src/test/java/net/coderodde/cskit/sorting/RunScannerTest.java
@@ -1,4 +1,6 @@
 package net.coderodde.cskit.sorting;
 
 import java.util.Comparator;
-import static net.coderodde.cski
+import static net.coderodde.cskit.Utilities.debugPrintArray;
+import org.junit.Test;
+import stati
