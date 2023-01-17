ee43570b3db4af3218502281e309ebc1d35883d2
oevevr <120677373+oevevr@users.noreply.github.com>
2023-01-17 07:12:12 +0000

    Entity

--- src/test/java/net/coderodde/cskit/sorting/RunScannerTest.java
+++ src/test/java/net/coderodde/cskit/sorting/RunScannerTest.java
@@ -3,4 +3,9 @@ package net.coderodde.cskit.sorting;
 import java.util.Comparator;
 import static net.coderodde.cskit.Utilities.debugPrintArray;
 import org.junit.Test;
-import stati
+import static org.junit.Assert.*;
+
+/**
+ * This class tests <code>RunScanner</code>.
+ *
+ * @author Rodio
